959f0e833c2d30d1d2078914b1e7df05c1bce199
Rxsch <danielrangosch2004@gmail.com>
2025-09-22 19:17:49 -0400

    Feat: Driver File was added.

--- TreasureCoordinates.java
+++ TreasureCoordinates.java
@@ -4,13 +4,19 @@ COP3503 Fall 2025
 Programming Assignment 2
 */
 public class TreasureCoordinates{
-public static void main(String[] args)
+    //Input: (123)
+    /*Output:
+    (1, 2.3)
+    (1, 23)
+    (1.2, 3)
+    (12, 3)
+    
+    */
+    public static void main(String[] args)
 {
 //Code goes here
     
-System.out.println("Hola");
-System.out.println("Test comment");
-    //determineCoordinates() 
+//determineCoordinates() 
 }
 }
 
