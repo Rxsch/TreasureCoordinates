f66e076414b2e188a35b1bdfc9ec3ba2904cc9b8
Rxsch <danielrangosch2004@gmail.com>
2025-09-22 19:01:43 -0400

    Project Start

---
+++ TreasureCoordinates.java
@@ -0,0 +1,14 @@
+/* Daniel Rangosch
+Dr. Steinberg
+COP3503 Fall 2025
+Programming Assignment 2
+*/
+public class TreasureCoordinates{
+public static void main(String[] args)
+{
+//Code goes here
+    
+System.out.println("Hola");
+    //determineCoordinates() 
+}
+}
