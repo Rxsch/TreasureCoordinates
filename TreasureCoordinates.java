f741f775cd3467454b42de0e2342495db15e5af5
Rxsch <danielrangosch2004@gmail.com>
2025-09-23 14:01:16 -0400

    Feat: Improve listSplitter and create decimalGenerator

--- TreasureCoordinates.java
+++ TreasureCoordinates.java
@@ -13,11 +13,12 @@ public class TreasureCoordinates{
     (1.2, 3)
     (12, 3)
     */
+    boolean isValid=0;
 //Main
     public static void main(String[] args)
 {
 //Boolean variable that checks if the number can be splitted into decimal
-boolean isValid=0;
+
     
 
  
@@ -30,7 +31,7 @@ boolean isValid=0;
     public ArrayList<String> determineCoordinates(String rawDigits)
 {
     //This takes out the parenthesis of the parameter String
-    String newList= rawDigits.substring(1,rawDigits.length());
+    String newList= rawDigits.substring(1,rawDigits.length() - 1);
     
     /*
     This list will store all the possible combinations. Form (x,y)
@@ -42,10 +43,34 @@ boolean isValid=0;
     return processedDigitsList;
 }
 
-/*
- * This funcion will split the newList into all the possible combinations.
- */
+
+  //This funcion will split the newList into all the possible combinations.
+
  public String listSpliter(String list){
+    String result;
+    //Iterate throught the list without parenthesis and make all the combination possibilities
+    for(int i=0;i<list.length();i++){
+        
+        String leftPart= list.substring(i);
+        String rightPart=list.substring(i+1, list.length()-1);
+    }
+result=leftPart+rightPart; //I dont know if this part is right but this is how I would do it
+    
+  //decimalGenerator
+ }
+
+ /*This function will put a decimal in all available spaces*/
+ public String decimalGenerator(String list)
+ {
+//It is more than one number. Add a decimal
+ if(list.length()>0)
+ {
+isValid=true;
+ }
+ if(isValid)
+ {
+//Start adding decimals
+ }
 
  }
 }
