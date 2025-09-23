4ea343fec04881a78a9bc43382801f92caa915b4
Rxsch <danielrangosch2004@gmail.com>
2025-09-23 13:00:30 -0400

    Feat: Comments and first methods

--- TreasureCoordinates.java
+++ TreasureCoordinates.java
@@ -3,20 +3,29 @@ Dr. Steinberg
 COP3503 Fall 2025
 Programming Assignment 2
 */
+import java.util.*;
+import java.util.ArrayList;
 public class TreasureCoordinates{
-    //Input: (123)
-    /*Output:
+    /*Input: (123)
+    Output:
     (1, 2.3)
     (1, 23)
     (1.2, 3)
     (12, 3)
-    
     */
     public static void main(String[] args)
 {
-//Code goes here
+/*
+This list will store all the possible combinations. Form (x,y)
+Each index will be a possible coordinate.
+*/
+ArrayList processedDigitsList=new ArrayList<>(String);
     
- public determineCoordinates(void)
+/*
+This function takes a String of digits in between parenthesis and
+uses backtracking to find all possible combinations
+*/
+ public ArrayList<String> determineCoordinates(String unprocessedDigits)
 {
 
 }
