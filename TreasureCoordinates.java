76a8a9a94f657aa760a7eb09f773230b0ff8b2e0
Rxsch <danielrangosch2004@gmail.com>
2025-09-24 16:27:16 -0400

    Final version

--- TreasureCoordinates.java
+++ TreasureCoordinates.java
@@ -2,9 +2,9 @@
  * Dr. Steinberg
  * COP3503 Fall 2025
  *Programming Assignment 2
-*/
+ */
 import java.util.*;
-public class TreasureCoordinates 
+public class TreasureCoordinates
 {
 
 	//Function to find all possible combinations adding a decimal
@@ -12,40 +12,40 @@ public class TreasureCoordinates
 	{
 		//Removes the parenthesis of the parameter String
 		String newList= rawDigits.substring(1,rawDigits.length() - 1);
-		
+
 		//ArrayList where all the combinations will be stored
 		ArrayList <String> processedDigitsList=new ArrayList<>();
 
-        //Process the array
-		listSplitterRecursive(newList,1 , processedDigitsList);
-		
+		//Process the array
+		listSplitterRecursive(newList,1, processedDigitsList);
+
 		return processedDigitsList;
 	}
 
 	//This funcion splits the newList into all the possible combinations.
-	public void listSplitterRecursive(String list, int commaPos, ArrayList<String> processedDigitsList) 
-    {
+	public void listSplitterRecursive(String list, int commaPos, ArrayList<String> processedDigitsList)
+	{
 		//Base case.
-		if(commaPos >= list.length()) 
+		if(commaPos >= list.length())
 		{
 			return;
 		}
-		//Split into left and right partss 
+		//Split into left and right partss
 		String leftPart= list.substring(0,commaPos);
 		String rightPart=list.substring(commaPos);
 
-        //Add decimals
+		//Add decimals
 		ArrayList<String> xCoordinate=decimalGenerator(leftPart);
 		ArrayList<String> yCoordinate=decimalGenerator(rightPart);
 
 		//Combine results into a coordinate
-		for (int j = 0; j < xCoordinate.size(); j++) 
-        {
-			for (int k = 0; k < yCoordinate.size(); k++) 
-            {
-					String x = xCoordinate.get(j);
-					String y = yCoordinate.get(k);
-					processedDigitsList.add("(" + x + ", " + y + ")");
+		for (int j = 0; j < xCoordinate.size(); j++)
+		{
+			for (int k = 0; k < yCoordinate.size(); k++)
+			{
+				String x = xCoordinate.get(j);
+				String y = yCoordinate.get(k);
+				processedDigitsList.add("(" + x + ", " + y + ")");
 			}
 		}
 
@@ -58,26 +58,26 @@ public class TreasureCoordinates
 	{
 		ArrayList<String> results = new ArrayList<>();
 
-		//Checks if the number can be splitted into decimal 
+		//Checks if the number can be splitted into decimal
 		boolean isValid= list.length() >1;
 
-		//Removes irrelevant zeros 
-		if(list.charAt(0)== '0') 
-        {
-			if(list.length()==1) 
-            {
+		//Removes irrelevant zeros
+		if(list.charAt(0)== '0')
+		{
+			if(list.length()==1)
+			{
 				results.add("0");
 			}
-			else if(list.charAt(list.length() - 1) != '0') 
-            {
+			else if(list.charAt(list.length() - 1) != '0')
+			{
 				results.add("0."+ list.substring(1));
 			}
 			return results;
 		}
 
-		//Part that handles regular cases 
-		//Start adding decimals 
-		if(isValid) 
+		//Part that handles regular cases
+		//Start adding decimals
+		if(isValid)
 		{
 			//Start at right of the first number and add "."
 			for(int i=1; i<list.length(); i++)
